e06f80e1dc8b7e1a4c20a5b7ff8bb6f5a8d91e7a
Chantelle Belic <chantelle.belic@gmail.com>
2018-12-14 11:15:17 -0800

    passing props from board to card with hardcoded data

--- src/components/Board.js
+++ src/components/Board.js
@@ -19,7 +19,7 @@ class Board extends Component {
   render() {
     return (
       <div>
-        Board
+      <Card quote="hello moto" emoji="smiley" />
       </div>
     )
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
-class Card extends Component {
-  render() {
-    return (
-      <div className="card">
-        Card
-      </div>
-    )
-  }
+
+const Card = (props) => {
+
+  const { quote, emoji } = props;
+
+  return (
+    <div className="card">
+      <p> {quote} {emoji} </p>
+    </div>
+  )
 }
 
 Card.propTypes = {
-
+  quote: PropTypes.string.isRequired,
+  emoji: PropTypes.string
 };
+
 
 export default Card;
